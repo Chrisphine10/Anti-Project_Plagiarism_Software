a58686e882710487c39df7d3dcdf6ca508b56517
PC <PC@192.168.84.82>
2019-11-02 18:41:33 -0700

    authentication files

---
+++ src/com/plag/Add_Lecturer_Servlet.java
@@ -0,0 +1,46 @@
+package com.plag;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.appsdeveloperblog.encryption.PasswordUtils;
+
+/**
+ * Servlet implementation class Add_Lecturer_Servlet
+ */
+@WebServlet("/Add_Lecturer_Servlet")
+public class Add_Lecturer_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Add_Lecturer_Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String fname = request.getParameter("first_name");
+		String lname = request.getParameter("last_name");
+		String email = request.getParameter("email");
+		String id_no = request.getParameter("id_no");
+		String passwordraw = request.getParameter("firstpassword");
+		String salt = PasswordUtils.getSalt(30);
+		String mySecurePassword = PasswordUtils.generateSecurePassword(passwordraw, salt);
+		RequestDispatcher rd = request.getRequestDispatcher("index.html");
+        rd.forward(request, response);
+	}
+
+}
