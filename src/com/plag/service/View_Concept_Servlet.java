3cb02c826d9115e32de27e32cbbbd30e18e2f581
PC <PC@pheene.mshome.net>
2019-11-18 03:01:46 -0800

    jsp and html pages

--- src/com/plag/service/View_Concept_Servlet.java
+++ src/com/plag/service/View_Concept_Servlet.java
@@ -2,17 +2,13 @@ package com.plag.service;
 
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.plag.dao.ConceptDao;
 import com.plag.model.Concept;
-
-import test.Emp;
 
 /**
  * Servlet implementation class ConceptHandler
